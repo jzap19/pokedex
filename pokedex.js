80a002bf40d5a336550a186c5da37d1daf5cfdb7
Jose Zapata <jz3255@gmail.com>
2018-04-30 17:57:45 -0400

    initial commit

---
+++ pokedex.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  let myButton = .querySelector('thekingsstatus')
+  let myButton2 = .querySelector('theelectricbird')
+  let myButton3 = .querySelector('darkbunny')
+
+  jQuery.get('https://pokeapi.co/api/v2/pokemon-form/145/')
+
+
+});
